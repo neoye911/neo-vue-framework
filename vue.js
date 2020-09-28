9e3ccaf24425e9bbdc3cf4fc0d3c3af6d9197e23
Neo <neo.ye@asknice.ly>
2020-09-29 10:38:34 +1300

    Set methods option for Vue class

--- vue.js
+++ vue.js
@@ -2,13 +2,26 @@
 class Vue {
 
     constructor(options) {
-        this.selectors = Object.keys(options)
-        this.options = options
-        
+        this.selectors = Object.keys(options.data)
+        this.data = options.data
+        if (Object.keys(options.methods).length > 0) {
+            Object.keys(options.methods).forEach(e => {
+                this[e] = options.methods[e]
+                this.bindMethod(e)
+            })
+        }
+        console.log('this', this)
         this.setDoms()
 
         // initialization
         this.render()
+    }
+
+    bindMethod(methodKey) {
+        let bindNodes = document.querySelectorAll(`[click='${methodKey}']`) || []
+        bindNodes.forEach(e => {
+            e.addEventListener('click', this[methodKey].bind(this))
+        })
     }
 
     setDoms() {
@@ -20,7 +33,7 @@ class Vue {
 
     render() {
         this.selectors.forEach(e => {
-            this.updateDom(e, this.options[e])
+            this.updateDom(e, this.data[e])
         })
     }
 
